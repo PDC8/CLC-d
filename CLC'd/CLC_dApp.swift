0a6d90948167144544ca47af2e434e3658cee140
PDC8 <peidongchen2004@gmail.com>
2025-05-14 22:39:22 -0400

    Initial commit

---
+++ CLC'd/CLC_dApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLC_dApp.swift
+//  CLC'd
+//
+//  Created by Peidong Chen on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CLC_dApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
