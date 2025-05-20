52be2e644763598a157f9521db9b31cbefe7500d
PDC8 <peidongchen2004@gmail.com>
2025-05-20 19:16:12 -0400

    added graphQL and alarm logic for daily questions

--- CLC'd/ViewModels/AlarmViewModel.swift
+++ CLC'd/ViewModels/AlarmViewModel.swift
@@ -52,41 +52,35 @@ class AlarmViewModel: ObservableObject {
         }
     }
     
-//    private func startSubmissionCheck(for alarm: Alarm) {
-////        guard let username = UserDefaults.standard.string(forKey: "LeetCodeUsername") else {
-////            return
-////        }
-//        let username = "peidongchen2004"
-//        //Poll every 30 sec
-//        Timer.scheduledTimer(withTimeInterval: 30, repeats: true) { [weak self] timer in
-//            self?.checkLatestSubmission(username: username, alarmTime: alarm.time) { solved in
-//                if solved {
-//                    timer.invalidate()
-//                    AudioManager.shared.stopSound()
-//                    alarm.isTriggered = false
-//                }
-//            }
-//        }
-//    }
+    private func isValidQuestionType(submissionTitleSlug: String, completion: @escaping (Bool) -> Void) {
+        guard let alarm = activeAlarm else {
+            completion(false)
+            return
+        }
+        switch alarm.problemType {
+        case "Daily":
+            getDailyQuestion { titleSlug in
+                completion(titleSlug == submissionTitleSlug)
+            }
+        default:
+            completion(false)
+        }
+    }
     
-    func checkLatestSubmission(username: String, alarmTime: Date, completion: @escaping (Bool) -> Void) {
-        var validTime = false
-        var validStatus = false
+    private func getDailyQuestion(completion: @escaping (String?) -> Void) {
         let query = """
-            query recentAcSubmissions($username: String!) {
-              recentAcSubmissionList(username: $username, limit: 1) {
-                timestamp
-                statusDisplay
-              }
+            query getDailyProblem {
+                activeDailyCodingChallengeQuestion {
+                    question{
+                        titleSlug
+                    }
+                }
             }
             """
-        let variables: [String: Any] = ["username": username]
-        let body: [String: Any] = ["query": query, "variables": variables]
+        let body: [String: Any] = ["query": query]
         
         guard let url = URL(string: "https://leetcode.com/graphql") else {
-            DispatchQueue.main.async {
-                completion(validTime && validStatus)
-            }
+            print("URL error")
             return
         }
         
@@ -98,16 +92,57 @@ class AlarmViewModel: ObservableObject {
         URLSession.shared.dataTask(with: request) { data, _, _ in
             guard let data = data,
                   let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
-                    DispatchQueue.main.async {
-                        completion(validTime && validStatus)
-                    }
+                    print("JSON error")
+                    return
+            }
+            if let dataDict = json["data"] as? [String: Any],
+               let dailyChallenge = dataDict["activeDailyCodingChallengeQuestion"] as? [String: Any],
+               let question = dailyChallenge["question"] as? [String: Any],
+               let titleSlug = question["titleSlug"] as? String {
+                completion(titleSlug)
+            } else {
+                print("Something went wrong decoding the JSON.")
+            }
+        }.resume()
+    }
+    
+    func checkLatestSubmission(username: String, alarmTime: Date, completion: @escaping (Bool) -> Void) {
+        var validTime = false
+        var validStatus = false
+        let query = """
+            query recentAcSubmissions($username: String!) {
+                recentAcSubmissionList(username: $username, limit: 1) {
+                    timestamp
+                    statusDisplay
+                    titleSlug
+                }
+            }
+            """
+        let variables: [String: Any] = ["username": username]
+        let body: [String: Any] = ["query": query, "variables": variables]
+        
+        guard let url = URL(string: "https://leetcode.com/graphql") else {
+            print("URL error")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body)
+        
+        URLSession.shared.dataTask(with: request) { data, _, _ in
+            guard let data = data,
+                  let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+                    print("JSON error")
                     return
             }
             if let dataDict = json["data"] as? [String: Any],
                let submissions = dataDict["recentAcSubmissionList"] as? [[String: Any]],
                let latestSubmission = submissions.first,
                let status = latestSubmission["statusDisplay"] as? String,
-               let timestamp = latestSubmission["timestamp"] as? String {
+               let timestamp = latestSubmission["timestamp"] as? String,
+               let submissionTitleSlug = latestSubmission["titleSlug"] as? String {
                 let submissionTime = Int(timestamp)!
                 let alarmUnixTime = Int(alarmTime.timeIntervalSince1970)
                 if submissionTime >= alarmUnixTime {
@@ -116,14 +151,13 @@ class AlarmViewModel: ObservableObject {
                 if status == "Accepted" {
                     validStatus = true
                 }
-                print(validTime, validStatus)
-            }
-            else {
+                self.isValidQuestionType(submissionTitleSlug: submissionTitleSlug) { validQuestion in
+                    DispatchQueue.main.async {
+                        completion(validTime && validStatus && validQuestion)
+                    }
+                }
+            } else {
                 print("Something went wrong decoding the JSON.")
-            }
-
-            DispatchQueue.main.async {
-                completion(validTime && validStatus)
             }
         }.resume()
     }
